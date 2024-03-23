02867767a4a4e19ae572e88b393d92b1e4ec708b
aguizzo <andres.leandro.guizzo@estudiantat.upc.edu>
2024-03-23 10:07:18 +0100

    added connection to mongoDB cluster

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,16 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
+
+const USER = process.env.NASA_API_USERNAME;
+const PASSWORD = process.env.NASA_API_PASSWORD;
+const MONGO_URL = `mongodb+srv://${USER}:${PASSWORD}@mongocluster.xcxkkse.mongodb.net/?retryWrites=true&w=majority&appName=nasa`;
+
 /**
  * HOW TO SET ENV VARIABLES ON WINDOWS:
  * set PORT=5000 && node...
@@ -24,7 +30,17 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+})
+
+mongoose.connection.on('error', err => {
+    console.error(err);
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
+
     await loadPlanetsData();
     server.listen(
         PORT, 
